53e3833e2762a91964a872f4e75350563919c5ce
User <sr0876392@otc.edu>
2018-10-23 22:49:50 -0500

    first page working

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
-// Start JS here
+var fName = document.myForm.fname;
+var lName = document.myForm.lname;
+var eMail = document.myForm.email;
+var phoneNum = document.myForm.phone;
+var radio = document.myForm.subscribe;
+var fnameErr = document.getElementById("fNameErr");
+var lnameErr = document.getElementById("lNameErr");
+var emailErr = document.getElementById("emailErr");
+var phoneErr = document.getElementById("phoneErr");
+var radioErr = document.getElementById("radioErr");
+var emailRgx = /\S+@\S+\.\S+/;
+var phoneRgx = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
+
+
+function Default (){
+    var formLen = document.myForm.length;
+    for (var i = 0; formLen > i; i++){
+        document.myForm[i].style.backgroundColor = "white";
+    }
+    fnameErr.style.display ="none";
+    lnameErr.style.display ="none";
+    emailErr.style.display ="none";
+    phoneErr.style.display ="none";
+}
+function validateForm(){
+
+    if (fName.value.length < 6){
+        fnameErr.style.display ="inline";
+        fName.style.backgroundColor = "pink";
+        fnameErr.innerHTML = "* Required fields are missing";
+        fName.focus();
+        return false;
+    }
+    if (lName.value.length < 6){
+        lnameErr.style.display ="inline";
+        lName.style.backgroundColor = "pink";
+        lnameErr.innerHTML = "* Required fields are missing";
+        lName.focus();
+        return false;
+    }
+    if (!emailRgx.test(eMail.value)){
+        emailErr.style.display ="inline";
+        eMail.style.backgroundColor = "pink";
+        emailErr.innerHTML = "* Required fields are missing";
+        eMail.focus();
+        return false;
+    }
+    if (!phoneRgx.test(phoneNum.value)){
+        phoneErr.style.display ="inline";
+        phoneNum.style.backgroundColor = "pink";
+        phoneErr.innerHTML = "* Required field is incorrect";
+        phoneNum.focus();
+        return false;
+    }
+    if(radio.value == ""){
+        radioErr.style.display ="inline";
+        radioErr.innerHTML = "* Please make a selection";
+        return false;
+    }
+    return true;
+}
+function Redirect(){
+    if (validateForm()== true){
+        document.myForm.submit()
+    }
+}
